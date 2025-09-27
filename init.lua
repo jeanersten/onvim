f02cc28c83d03fccaab9b064ada6616d4baa03d3
Ahmad Fauzan <gavier.exe@gmail.com>
2025-09-28 01:03:00 +0700

    feat(tools): implement custom explorer extension
    - add 'lua/module/explorer.lua'
    - add 'lua/util/system.lua'
    
    Neovim's built-in file explorer, Netrw, is a bit incomplete. This new custom explorer is meant to make exploring better.
    It also includes a utility file for easier abstraction of some system-specific things.
    That utility file may be used for future extensions.

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ require("core.global")
 require("core.keymap")
 require("core.option")
 
+require("module.explorer").setup({})
 require("module.statusline").setup({})
 require("module.tabline").setup({})
 require("module.terminal").setup({})
